171e50485203704b9a8dcc78dfea7945b14a33f5
joshuabrrs <jgb16@my.fsu.edu>
2018-04-12 12:57:31 -0400

    goes to chat after registering

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -38,6 +38,7 @@ class RegisterViewController: UIViewController {
             }
             else{
                 print("success!")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
         
